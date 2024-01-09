16cea47a1cd30a9048a73c75c4449306d6e6eef7
navin-git <nabinkharel142@gmail.com>
2024-01-09 17:00:47 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div className="grid grid-cols-3 gap-2 p-4">
+      <div className="card">content</div>
+      <div className="card">content</div>
+      <div className="card">content</div>
+    </div>
+  );
+};
+
+export default App;
