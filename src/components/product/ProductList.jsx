49b534108bcce75ca7f408f36741615002f086a7
navin-git <nabinkharel142@gmail.com>
2024-01-14 17:07:00 +0545

    feat: array list in jsx

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../common/Card";
 
 const ProductList = () => {
+  const [list, setList] = useState([
+    {
+      title: "nabin",
+      dec: "this is nabin",
+      id: 1,
+      image:
+        "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
+    },
+    {
+      title: "jubin",
+      dec: "this is jubin",
+      id: 1,
+      image:
+        "https://slp-statics.astockcdn.net/static_assets/staging/24winter/home/curated-collections/card-2.jpg?width=580",
+    },
+    {
+      title: "Saroj",
+      dec: "this is saroj",
+      id: 1,
+      image:
+        "https://media.istockphoto.com/id/1225173869/photo/house-boat-anchored-in-lake-with-jungle-background-backwaters-kerala-india.jpg?s=612x612&w=0&k=20&c=uo-bsRQjhlT9AgeWBs_pkSvHQwStCelMC75EUpzwjHU=",
+    },
+    {
+      title: "Suraj",
+      dec: "this is Suraj",
+      id: 1,
+    },
+  ]);
   return (
-    <div className="grid grid-cols-3 gap-3">
-      <Card title="Hills" dec="hello I am hills" />
-      <Card title="Hills" dec="hello I am hills" />
-      <Card title="Hills" dec="hello I am hills" />
-      <Card title="Hills" dec="hello I am hills" />
-      <Card title="Hills" dec="hello I am hills" />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      {Array.isArray(list) &&
+        list.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              dec={item?.dec}
+              pic={item?.image}
+              title={item?.title}
+            />
+          );
+        })}
     </div>
   );
 };
--- src/components/static/Navbar.jsx
+++ src/components/static/Navbar.jsx
@@ -1,29 +1,53 @@
 import React from "react";
 
 const Navbar = ({ setIsOpen }) => {
+  const navList = [
+    {
+      title: "home",
+      path: "/home",
+    },
+    {
+      title: "work",
+      path: "/work",
+    },
+    {
+      title: "projects",
+      path: "/projects",
+    },
+    {
+      title: "about",
+      path: "/about",
+    },
+    {
+      title: "contact",
+      path: "/contact",
+    },
+  ];
   return (
-    <nav className="bg-orange-100 py-4 px-[5%] flex items-center gap-4 justify-between">
+    <nav className="bg-orange-100 sticky top-0 py-4 px-[5%] flex items-center gap-4 justify-between">
       <img
         src="https://1stwebdesigner.com/wp-content/uploads/2020/01/1stwebdesigner-logo-2020.png"
         alt=""
         className="h-10"
       />
-      <ul className="flex justify-end items-center gap-4 text-gray-700 font-medium">
-        <li className="cursor-pointer">home</li>
-        <li className="cursor-pointer">work</li>
-        <li className="cursor-pointer">project</li>
-        <li className="cursor-pointer">about</li>
-        <li className="cursor-pointer">contact</li>
-        <li className="cursor-pointer">
-          <button
-            onClick={() => {
-              setIsOpen((pre) => !pre);
-            }}
-          >
-            menu
-          </button>
-        </li>
+      <ul className="sm:flex hidden justify-end items-center gap-4 text-gray-700 font-medium">
+        {Array.isArray(navList) &&
+          navList.map((item, index) => {
+            return (
+              <li key={index} className="cursor-pointer text-red-500">
+                <a href={item?.path}>{item?.title}</a>
+              </li>
+            );
+          })}
       </ul>
+      <button
+        className="block sm:hidden"
+        onClick={() => {
+          setIsOpen((pre) => !pre);
+        }}
+      >
+        menu
+      </button>
     </nav>
   );
 };
