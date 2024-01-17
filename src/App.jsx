014a240b3563893f24987bdedfdac130c5ed2ebe
navin-git <nabinkharel142@gmail.com>
2024-01-17 16:56:37 +0545

    feat: input state

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,40 @@ import Addproduct from "./components/add-product/Addproduct";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  console.log(email, name);
   return (
     <main>
       <Navbar setIsOpen={setIsOpen} />
       <Sidebar isOpen={isOpen} />
       <section className="py-10 px-[5%]">
         <ProductList />
-        <Counter />
-        <Addproduct />
+        {/* <Counter /> */}
+        {/* <Addproduct /> */}
+
+        <div className="productInput">
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            value={name}
+            id="name"
+            onChange={(e) => {
+              setName(e?.target?.value);
+            }}
+          />
+        </div>
+        <div className="productInput">
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            value={email}
+            id="email"
+            onChange={(e) => {
+              setEmail(e?.target?.value);
+            }}
+          />
+        </div>
       </section>
       <Footer />
     </main>
