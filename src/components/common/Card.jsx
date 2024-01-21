d039b3a16fd4048aa0e8e50f910fa8acd18f281e
navin-git <nabinkharel142@gmail.com>
2024-01-21 17:20:46 +0545

    feat: reduce and use memo

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -2,12 +2,19 @@ const Card = ({
   pic = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDM43pAzXkKe97pmZXJ_HEX68fQMQc0J_hRmmzxOmWDz_BPjv-rR5rDYXPWqfKdwNHKs&usqp=CAU",
   title = "",
   dec = "",
+  rate,
+  qty,
 }) => {
   return (
     <div className="card">
       <img src={pic} alt="" className="h-[250px] w-full object-cover" />
       <div className="mt-2">
-        <h1 className="text-xl font-semibold">{title}</h1>
+        <div className="flex gap-2 items-center justify-between">
+          <h1 className="text-xl font-semibold">{title}</h1>
+          <div>
+            {rate} * {qty}
+          </div>
+        </div>
         <p className="text-gray-600">{dec}</p>
       </div>
     </div>
--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Card from "../common/Card";
 import Title from "../common/Title";
 
@@ -8,29 +8,61 @@ const ProductList = () => {
       title: "nabin",
       dec: "this is nabin",
       id: 1,
+      price: 200,
+      qty: "4",
       image:
         "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
     },
     {
       title: "jubin",
       dec: "this is jubin",
-      id: 1,
+      id: 2,
+      price: 400,
+      qty: 8,
       image:
         "https://slp-statics.astockcdn.net/static_assets/staging/24winter/home/curated-collections/card-2.jpg?width=580",
     },
     {
       title: "Saroj",
       dec: "this is saroj",
-      id: 1,
+      id: 3,
+      price: 100,
+      qty: 3,
       image:
         "https://media.istockphoto.com/id/1225173869/photo/house-boat-anchored-in-lake-with-jungle-background-backwaters-kerala-india.jpg?s=612x612&w=0&k=20&c=uo-bsRQjhlT9AgeWBs_pkSvHQwStCelMC75EUpzwjHU=",
     },
     {
       title: "Suraj",
       dec: "this is Suraj",
-      id: 1,
+      id: 4,
+      price: 300,
+      qty: 400,
     },
   ]);
+  const [show, setShow] = useState("");
+
+  // const [totalqty, setTotalQty] = useState(0);
+  // useEffect(() => {
+  //   setTotalQty(list.reduce((acc, item) => acc + item?.qty, 0));
+  // }, [list]);
+  const [mapData, setMapData] = useState([]);
+
+  const totalqty = useMemo(() => {
+    return mapData.reduce((acc, item) => acc + parseInt(item?.qty), 0);
+  }, [mapData]);
+
+  const totalAmount = useMemo(() => {
+    return mapData.reduce((acc, item) => acc + item?.price * item?.qty, 0);
+  }, [mapData]);
+
+  const filterList = useMemo(() => {
+    return list.filter((item) => item?.price > 200);
+  }, [mapData]);
+
+  useEffect(() => {
+    setMapData(show === "high" ? filterList : list);
+  }, [list, show]);
+
   // useEffect(() => {
   //   alert("hello");
   // }, [list]);
@@ -38,35 +70,58 @@ const ProductList = () => {
     <div className="space-y-10">
       <div className="flex items-center justify-between">
         <Title title={"Product List"} />
-        <button
-          onClick={() => {
-            setList((pre) => {
-              return [
-                ...pre,
-                {
-                  title: "Suraj",
-                  dec: "this is Suraj",
-                  id: 1,
-                },
-              ];
-            });
-          }}
-        >
-          add
-        </button>
+        <div className="flex gap-4">
+          <button
+            onClick={() => {
+              setList((pre) => {
+                return [
+                  ...pre,
+                  {
+                    title: "Suraj",
+                    dec: "this is Suraj",
+                    id: list?.length + 1,
+                    price: 300,
+                    qty: 400,
+                  },
+                ];
+              });
+            }}
+          >
+            add
+          </button>
+          <button
+            onClick={() => {
+              setShow("high");
+            }}
+          >
+            high price
+          </button>
+          <button
+            onClick={() => {
+              setShow("all");
+            }}
+          >
+            All
+          </button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        {Array.isArray(list) &&
-          list.map((item, index) => {
-            return (
-              <Card
-                key={index}
-                dec={item?.dec}
-                pic={item?.image}
-                title={item?.title}
-              />
-            );
-          })}
+        {mapData.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              dec={item?.dec}
+              pic={item?.image}
+              title={item?.title}
+              rate={item?.price}
+              qty={item?.qty}
+            />
+          );
+        })}
+      </div>
+      <div className="flex flex-col items-end">
+        <p>total qty : {totalqty}</p>
+        <p>total Amount : {totalAmount}</p>
       </div>
     </div>
   );
